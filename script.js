c361b5e01bc4ce1434ddeb71e0d6f7b6fd933548
ZONA APLIKASI <zonaplikasii@gmail.com>
2025-05-24 00:38:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let clicked = false;
 const downloadBtn = document.getElementById("downloadBtn");
 const customAlert = document.getElementById("customAlert");
 
-function showAlert(message, duration = 2000) {
+function showAlert(message, duration = 5000) {
   customAlert.textContent = message;
   customAlert.classList.add("show");
 
